cae162e60f647f8a3ed90c612d5cfe3e7ab3b6fd
Uttpol roy <99748784+utpolra1@users.noreply.github.com>
2025-09-09 10:05:40 +0600

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,43 +1,42 @@
-// movie.js
-export const MOVIES = [
-  {
-    id: 'm6',
-    title: 'Baaghi 4 ',
-    year: 2025,
-    genre: 'Action',
-    poster: 'https://www.filmyzilla20.com/files/images/Baaghi-4-(2025)-hindi-movie.jpg?time=1757354400',
-    streamUrl: 'https://d-s.io/e/gqqlgdkjlst1',
-    downloadUrl: 'https://www.filmyzilla20.com/downloads/32263/1/server_1/',
-    quality: 'HD'
-  },
-  {
-    id: 'm3',
-    title: 'Jawan',
-    year: 2023,
-    genre: 'Action',
-    poster: 'https://image.tmdb.org/t/p/w500/b5mS8Aqj8WcN6iYG3PaY5E9OQj3.jpg',
-    streamUrl: 'https://www.youtube.com/embed/MWOlnZSnXJo',
-    downloadUrl: 'https://filesamples.com/samples/video/mp4/sample_1280x720_surfing_with_audio.mp4',
-    quality: 'HD'
-  },
-  {
-    id: 'm4',
-    title: 'La La Land',
-    year: 2016,
-    genre: 'Romance',
-    poster: 'https://image.tmdb.org/t/p/w500/uDO8zWDhfWwoFdKS4fzkUJt0Rf0.jpg',
-    streamUrl: 'https://www.youtube.com/embed/0pdqf4P9MB8',
-    downloadUrl: 'https://filesamples.com/samples/video/mp4/sample_960x540.mp4',
-    quality: 'HD'
-  },
-  {
-    id: 'm5',
-    title: 'Drishyam 2',
-    year: 2022,
-    genre: 'Thriller',
-    poster: 'https://image.tmdb.org/t/p/w500/kSvzJsteUms9UpAJZmomr06Pq4j.jpg',
-    streamUrl: 'https://www.youtube.com/embed/G9MK8iB4FNM',
-    downloadUrl: 'https://filesamples.com/samples/video/mp4/sample_640x360.mp4',
-    quality: 'HD'
-  }
+const MOVIES = [
+  {
+    id: 'm6',
+    title: 'Baaghi 4',
+    year: 2025,
+    genre: 'Action',
+    poster: 'https://www.filmyzilla20.com/files/images/Baaghi-4-(2025)-hindi-movie.jpg?time=1757354400',
+    streamUrl: 'https://d-s.io/e/gqqlgdkjlst1',
+    downloadUrl: 'https://www.filmyzilla20.com/downloads/32263/1/server_1/',
+    quality: 'HD'
+  },
+  {
+    id: 'm3',
+    title: 'Jawan',
+    year: 2023,
+    genre: 'Action',
+    poster: 'https://image.tmdb.org/t/p/w500/b5mS8Aqj8WcN6iYG3PaY5E9OQj3.jpg',
+    streamUrl: 'https://www.youtube.com/embed/MWOlnZSnXJo',
+    downloadUrl: 'https://filesamples.com/samples/video/mp4/sample_1280x720_surfing_with_audio.mp4',
+    quality: 'HD'
+  },
+  {
+    id: 'm4',
+    title: 'La La Land',
+    year: 2016,
+    genre: 'Romance',
+    poster: 'https://image.tmdb.org/t/p/w500/uDO8zWDhfWwoFdKS4fzkUJt0Rf0.jpg',
+    streamUrl: 'https://www.youtube.com/embed/0pdqf4P9MB8',
+    downloadUrl: 'https://filesamples.com/samples/video/mp4/sample_960x540.mp4',
+    quality: 'HD'
+  },
+  {
+    id: 'm5',
+    title: 'Drishyam 2',
+    year: 2022,
+    genre: 'Thriller',
+    poster: 'https://image.tmdb.org/t/p/w500/kSvzJsteUms9UpAJZmomr06Pq4j.jpg',
+    streamUrl: 'https://www.youtube.com/embed/G9MK8iB4FNM',
+    downloadUrl: 'https://filesamples.com/samples/video/mp4/sample_640x360.mp4',
+    quality: 'HD'
+  }
 ];
